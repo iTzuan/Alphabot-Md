f87fa14e401fe9cf62be61e51be2bf3d0d4834d5
iTzuan <71885820+iTzuan@users.noreply.github.com>
2023-02-28 22:55:53 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ const {
 } = require(`./language`)
 
 //OpenAi || ChatGpt
-global.OpenAIKey = "YOUR_OPENAI_KEY" //login to https://beta.openai.com/ to get apikey
+global.OpenAIKey = "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" //login to https://beta.openai.com/ to get apikey
 
 // Website Api (jgn di ganti tomlol)
 global.APIs = {
